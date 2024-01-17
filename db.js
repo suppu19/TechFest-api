b828203726e8fb340969cbc6f0ca63e870cd3075
suppu19 <supraja.angirekula@gmail.com>
2024-01-17 22:08:14 +0530

    modified

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'Pycube123$',
+    user: '',
+    password: '',
     port: 3306,
     database : 'nodeAuth'
 });
